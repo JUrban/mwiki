217991aa18a1a1dae0661950a1ca96e132260c45
Josef Urban <urban@urban.(none)>
2009-09-14 19:48:45 +0200

    hopefully did the s/ right now

--- coq.pm
+++ coq.pm
@@ -35,9 +35,7 @@ sub htmlize (@) {
 
 
 # Run coqdoc on $content, giving the file name $pname.v, return the html
-# Creates temp dir in /tmp, which should be removed at some point (after debuging)
-# An evil hack is now done to fix the local html links - just by looking at the host name (hair-dryer :-)
-# - either get more info from ikiwiki API, or hack coqdoc to do the links locally
+# Creates temp dir in /tmp, which should be removed at some point (after debuging).
 sub coqdoc {
     my ($pname, $content) = @_;
     my $ProblemFile = $pname . '.v';
@@ -57,11 +55,10 @@ sub coqdoc {
     printf(PFH "%s",$content);
     close(PFH);
 
-    my $host='hair-dryer';
+    my $result = `cd $TemporaryProblemDirectory; /home/urban/corn_stable/CoRN/bin/CoRNc $ProblemFile; coqdoc --no-index --body-only --stdout $ProblemFile |tee $ProblemFile.html`;
+    $result =~ s/\"[a-zA-Z0-9_-]+\.html\#/\"\#/g;
 
-    my $result = `cd $TemporaryProblemDirectory; /home/urban/corn_stable/CoRN/bin/CoRNc $ProblemFile; coqdoc --no-index --body-only --stdout $ProblemFile`;
-    $result =~ s/$host(.*[/])[^/]+.html\#/$host$1\#/g;
-    system("rm -rf $TemporaryProblemDirectory");
+#    system("rm -rf $TemporaryProblemDirectory");
 
     return $result;
 }
