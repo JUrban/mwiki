cb92ef1ca51e363b991f60ca06c0a866c701e772
Josef Urban <urban@urban.(none)>
2009-09-18 21:03:31 +0200

    trying to do all things inside the srcdir

--- coq.pm
+++ coq.pm
@@ -40,18 +40,34 @@ sub coqdoc {
     my ($pname, $directories, $content) = @_;
     my $ProblemFile = $pname . '.v';
     my $GlobFile = $pname . '.glob';
-    my $TemporaryProblemDirectory = mkdtemp("/tmp/coq_XXXX");
-    my $PidNr = $$;
+    my $result = "";
+
+    # set to 0 for doing things in $config{srcdir} (preferred),
+    # set to 1 for doing things in /tmp
+    my $DoTmp = 0;
+
+    if($DoTmp == 1)
+    {
+	my $TemporaryProblemDirectory = mkdtemp("/tmp/coq_XXXX");
+
+	system("chmod 0777 $TemporaryProblemDirectory");
 
 
-    system("chmod 0777 $TemporaryProblemDirectory");
+	open(PFH, ">$TemporaryProblemDirectory/$ProblemFile") or die "$ProblemFile not writable";
+	printf(PFH "%s",$content);
+	close(PFH);
 
+	$result = `cd $TemporaryProblemDirectory; /home/urban/corn_stable/CoRN/bin/CoRNc $ProblemFile; coqdoc --no-index --body-only --stdout $ProblemFile |tee $ProblemFile.html; cp $GlobFile $config{srcdir}/$directories$GlobFile`;
+    }
+    else
+    {
+	open(PFH, ">$config{srcdir}/$directories$ProblemFile") or die "$ProblemFile not writable";
+	printf(PFH "%s",$content);
+	close(PFH);
 
-    open(PFH, ">$TemporaryProblemDirectory/$ProblemFile") or die "$ProblemFile not writable";
-    printf(PFH "%s",$content);
-    close(PFH);
+	$result = `cd $config{srcdir}/$directories; /home/urban/corn_stable/CoRN/bin/CoRNc $ProblemFile; coqdoc --no-index --body-only --stdout $ProblemFile |tee $ProblemFile.html`;
+    }
 
-    my $result = `cd $TemporaryProblemDirectory; /home/urban/corn_stable/CoRN/bin/CoRNc $ProblemFile; coqdoc --no-index --body-only --stdout $ProblemFile |tee $ProblemFile.html; cp $GlobFile $config{srcdir}/$directories$GlobFile`;
     $result =~ s/\"[a-zA-Z0-9_-]+\.html\#/\"\#/g;
 
 #    system("rm -rf $TemporaryProblemDirectory");
