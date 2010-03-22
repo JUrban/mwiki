286364493b2981a35d7eb8347f7c41e4f0d0ff48
Jesse Alama <alama@pequeno.(none)>
2010-03-22 01:40:57 +0000

    Um.

--- install-jesse.sh
+++ install-jesse.sh
@@ -1,17 +1,20 @@
-#!//bin/sh
+#!/bin/sh
 
-repos_base=/tmp/mwiki
+repos_base=/var/cache/mwiki
 mizfiles=/home/alama/share/mizar
 mizbin=/home/alama/src/mizar/mizar-source-git
+xsl4miz=/home/alama/src/mizar/xsl4mizar
+num_articles=5
 
-if [ "$1" -eq "clean" ]; then
-    make -f Makefile.install clean \
-	REPOS_BASE=$repos_base \
-	MIZFILES=$mizfiles \
-	MIZBIN=$mizbin;
+if [ "$1" = "clean" ]; then
+    targets='clean'
 else
-    make -f Makefile.install html repo-export \
+    targets='mwiki-user html sandbox repo-export gitweb'
+fi
+
+make -f Makefile.install $targets \
 	REPOS_BASE=$repos_base \
 	MIZFILES=$mizfiles \
-	MIZBIN=$mizbin;
-fi
+	MIZBIN=$mizbin \
+	XSL4MIZ=$xsl4miz \
+	NUM_ARTICLES=$num_articles;
