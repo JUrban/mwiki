7ab4171afd7e1217782e6070c841bf791cb82ee7
Jesse Alama <alama@stanford.edu>
2010-03-31 15:39:43 +0100

    Factor our index CSS into a separate file.

--- mkmmlindex.pl
+++ mkmmlindex.pl
@@ -62,12 +62,7 @@ my $header=<<END;
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
 <head>
 <title>Mizar Mathematical Library (current wiki state), Index of Identifiers</title>
-<style type="text/css">
-	body {font-family: monospace; margin: 0px;}
-	.wikiactions ul { background-color: DarkSeaGreen ; color:blue; margin: 0; padding: 6px; list-style-type: none; border-bottom: 1px solid #000; }
-	.wikiactions li { display: inline; padding: .2em .4em; }
-        div.index {padding-left: 3mm;}
-</style>
+<style type="text/css" src="index.css"/>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
