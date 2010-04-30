5e36cb3ec65110dbc4eea60814121a5193e29a11
Jesse Alama <jesse.alama@gmail.com>
2010-04-30 17:28:54 +0100

    Add slots for location and creation time to the notebook class.
    Give them initforms and accessors.

--- lisp/notebook.lisp
+++ lisp/notebook.lisp
@@ -4,6 +4,10 @@
 (defclass notebook ()
   ((owner :type user)
    (title :type string)
-   (repo :type repo)))
+   (repo :type repo)
+   (location :type pathname
+	     :accessor notebook-location)
+   (creation-time :initform (get-universal-time)
+		  :accessor notebook-creation-time)))
 
 ;;; notebook.lisp ends here
