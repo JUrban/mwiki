ab100eb495e9f3aa148f2165b9bd556e04018b06
Jesse Alama <jesse.alama@gmail.com>
2010-04-30 17:36:07 +0100

    Add documentation to the AS-HTML and VERIFY generic functions.

--- lisp/article.lisp
+++ lisp/article.lisp
@@ -10,9 +10,16 @@
    (html :initform nil
 	 :accessor article-html)))
 
-(defgeneric verify ((article article)))
+(defgeneric verify (article)
+  (:documentation 
+   "Test whether the content of the article is logically valid."))
 
-(defgeneric as-html ((article article)))
+(defgeneric as-html (article)
+  (:documentation
+   "An HTML representation of the article.  The value is a string.
+
+We may want to explore later other representations (structured sexps,
+rather than strings, for example)."))
 	       
 
 ;;; article.lisp ends here
