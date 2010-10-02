b567706328155499c97a5afd903a891d2e515dc2
Josef Urban <urban@urban.(none)>
2010-10-02 22:22:39 +0200

    install script for atom

---
+++ install-urban1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+repos_base=/var/cache/fwiki
+mizfiles=/home/urban/mizinst/7.11.05_4.133.1080
+xsl4miz=/home/urban/gr/xsl4mizar
+num_articles=5
+
+
+if test -z "$1"; then
+    targets='repos'
+else
+    targets=$1
+fi
+
+make -f Makefile.smallinstall $targets \
+    REPOS_BASE=$repos_base \
+    MIZFILES=$mizfiles \
+    XSL4MIZ=$xsl4miz \
+    NUM_ARTICLES=$num_articles \
+    PUBLIC_MWIKI_USER=www-data \
+    MAKEJOBS=2 \
+    WIKIHOST=localhost \
+    REPO_NAME=foo1
+
