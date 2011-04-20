c4b10406870dc4fc01e88d039a5984eefd740c81
Josef Urban <urban@eee.(none)>
2011-04-20 21:55:17 +0200

    outsourcing constants to mw_common

--- mw_common.pm
+++ mw_common.pm
@@ -1,13 +1,27 @@
- package mw_common;
+package mw_common;
 
  ## The common math wiki finctions should be moved here
 
- use strict;
- use warnings;
- use File::Basename;
- use Cwd;
+use strict;
+use warnings;
+use File::Basename;
+use Cwd;
+use Exporter qw(import);
+our @EXPORT_OK = qw(MWUSER REPO_NAME MW_BTRFS GITWEB_ROOT);
 
 
+# the MWUSER - everything is now in his gitolite, this should be in sync with Makefile.smallinstall
+use constant MWUSER => "@@MWUSER@@";
+
+# the REPO_NAME - sync with Makefile.smallinstall, all gitweb repos dwell bellow this dir
+use constant REPO_NAME	  => "@@REPO_NAME@@";
+
+# do we use the btrfs cloning?- sync with smallinstall
+use constant MW_BTRFS	  => "@@MW_BTRFS@@";
+
+# gitweb
+
+use constant GITWEB_ROOT   => "@@GITWEB_ROOT@@";
 
 # assumes the right directory
 sub set_git_var
